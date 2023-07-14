f6febfadd4cd46fd562bf160f59eaff57d0eb058
Przemyslaw Szczygiel <przemix03@o2.pl>
2023-07-14 18:39:06 +0200

    initial commit with project config

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+export const App: React.FC = () => {
+  return <div className="appWrapper">Hello World without CRA!{process.env.NODE_ENV}</div>;
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const prod = process.env.NODE_ENV === 'production';
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const dotenv = require('dotenv');
+const webpack = require('webpack');
+
+
+// non secret .env vars
+const dotenvVariables = () => {
+  const env = dotenv.config().parsed;
+
+  const envKeys = Object.keys(env).reduce((prev, next) => {
+    prev[`process.env.${next}`] = JSON.stringify(env[next]);
+    return prev;
+  }, {});
+
+  return new webpack.DefinePlugin(envKeys);
+};
+
+
+module.exports = {
+  mode: prod ? 'production' : 'development',
+  entry: './src/index.tsx',
+  output: {
+    path: __dirname + '/dist/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        resolve: {
+          extensions: ['.ts', '.tsx', '.js', '.json'],
+        },
+        use: 'ts-loader',
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  devtool: prod ? undefined : 'source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+    new MiniCssExtractPlugin(),
+    dotenvVariables(),
+  ],
+};
